3837d98a8c2d0df5a0e04649c65836f6ccf8ebb0
kaiismith <thangdd.bi12-406@st.usth.edu.vn>
2024-05-23 14:41:37 +0700

    auth

---
+++ acladvance/src/main/java/org/usth/ict/ulake/acladvance/acladvance/GreetingResource.java
@@ -0,0 +1,52 @@
+package org.usth.ict.ulake.acladvance.acladvance;
+
+import io.smallrye.mutiny.Uni;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+
+    @Inject
+    GreetingService greetingService;
+
+    @GET
+    @Path("/cache/{key}")
+    public String getCache(@PathParam("key") String key) {
+        return greetingService.get(key);
+    }
+
+    @GET
+    @Path("/cache/reactive")
+    @Produces(MediaType.APPLICATION_JSON)
+//    public Uni<List<String>> getReactive() {
+//        return greetingService.getReactive();
+//    }
+
+    public void getReactive(@Suspended AsyncResponse asyncResponse) {
+        greetingService.getReactive()
+                .subscribe().with(
+                        result -> asyncResponse.resume(Response.ok(result).build()),
+                        failure -> asyncResponse.resume(Response.serverError().entity(failure.getMessage()).build())
+                );
+    }
+
+    @POST
+    @Path("/cache")
+    public PersonCache putCache(PersonCache personCache) {
+        greetingService.set(personCache.key, personCache.value);
+        return personCache;
+    }
+}
---
+++ acladvance/src/main/java/org/usth/ict/ulake/acladvance/acladvance/PersonCache.java
@@ -0,0 +1,14 @@
+package org.usth.ict.ulake.acladvance.acladvance;
+
+public class PersonCache {
+
+    public String key;
+    public String value;
+
+    public PersonCache(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public PersonCache() {}
+}
